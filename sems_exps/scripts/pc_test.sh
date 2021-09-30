c2ed89f951e0588726cfd9fc1a4db53b0870ef10
Moscowsky Anton <moscowskyad@gmail.com>
2021-09-30 19:18:54 +0300

    added config for PC example

--- sems_exps/scripts/pc_test.sh
+++ sems_exps/scripts/pc_test.sh
@@ -1,1 +1,7 @@
-rosrun extended_object_detection offline_image ../share/PC.jpg ../config/sems_experiments.xml ../share/PC_detected.jpg 
+for EXP in all sk sm km
+do
+    for TYPE in source hard soft75 soft5 soft25 soft1 soft0
+    do
+        rosrun extended_object_detection offline_image ../share/PC.jpg ../config/$EXP-$TYPE.xml ../share/$EXP-$TYPE.png 
+    done
+done
