720fb26fa8a60f70c06534b0eb3595c79128e235
Moscowsky Anton <moscowskyad@gmail.com>
2021-10-01 13:23:33 +0300

    needed images collected!

--- sems_exps/scripts/mouse_experiment.py
+++ sems_exps/scripts/mouse_experiment.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 import rospy
 from extended_object_detection.msg import SimpleObjectArray
 
+from cv_bridge import CvBridge
+import cv2
+from sensor_msgs.msg import Image
+
 class MouseExperiment(object):
     def __init__(self):
         rospy.init_node('mouse_experiment')
@@ -14,11 +18,27 @@ class MouseExperiment(object):
         
         self.cnt_object = rospy.get_param('~cnt_object', 'Colors')
         
+        self.stop_frames = rospy.get_param('~stop_frames', [10, 38, 70])
+        
         self.save_path = rospy.get_param('~save_path', '/tmp')        
         self.scores = [[], []]
         self.cnt = []
         
+        
+        
         rospy.Subscriber('simple_objects', SimpleObjectArray, self.so_cb)
+        
+        if len(self.stop_frames):
+            rospy.Subscriber('detected_image', Image, self.im_cb)
+            self.cv_bridge = CvBridge()
+            self.cntr = 0
+            
+    def im_cb(self, msg):
+        if self.cntr in self.stop_frames:
+            rgb_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+            cv2.imwrite(self.save_path+f"/stop_frame_{self.cntr}.jpg", rgb_image)
+        
+        self.cntr+=1
         
     def so_cb(self, msg):
         for id, obj_name in enumerate(self.compare_objects):            
@@ -48,7 +68,7 @@ class MouseExperiment(object):
             plt.legend()            
             plt.pause(0.1)
             
-        plt.savefig(self.save_path+"/scores.png")
+        plt.savefig(self.save_path+"/scores.jpg")
         print(f"Saved image to {self.save_path+'/scores.jpg'}")
 
 if __name__ == '__main__':        
